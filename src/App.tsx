44b1da9da8c17e273564976c9babc88a2160259a
Ian Henderson <ianhenderson@protonmail.com>
2023-02-05 20:13:14 -0700

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ export default function App() {
     dispatchRequest,
     dispatchSuccess,
   ] = usePagination<Work & { isInWishList?: boolean }>();
+
   const [wishListMap, setWishListMap] = useState<{ [key: string]: boolean }>(
     {}
   );
